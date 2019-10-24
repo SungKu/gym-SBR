2a8a1ef68f8e81c363aa065e66d307627d1a5574
SungKu <33339044+SungKu@users.noreply.github.com>
2019-10-24 18:46:11 +0900

    Update gym_SBR_env1.py

--- gym_SBR/envs/gym_SBR_env1.py
+++ gym_SBR/envs/gym_SBR_env1.py
@@ -128,7 +128,7 @@ class SbrEnv1(gym.Env):
         t, soln, x_last, kla_memory1, So_memory1, sp_memory1, t_memory1, kla_memory2, So_memory2, sp_memory2, t_memory2, kla_memory3, So_memory3, sp_memory3, t_memory3, kla_memory4, So_memory4, sp_memory4, t_memory4, kla_memory5, So_memory5, sp_memory5, t_memory5, kla_memory8, So_memory8, sp_memory8, t_memory5,Qeff,Qw = \
         SBR.run(WV, IV, t_ratio, influent_mixed, DO_control_par,x, DO_setpoints)        
 
-        return  t,soln, x_last, t_memory1, sp_memory1, So_memory1, t_memory2, sp_memory2, So_memory2, t_memory3, sp_memory3, So_memory3, t_memory4, sp_memory4, So_memory4, t_memory5, sp_memory5, So_memory5,   t_memory8, sp_memory8, So_memory8,    kla_memory1, kla_memory2, kla_memory3, kla_memory4, kla_memory5, kla_memory8,   Qeff,Qw
+        return  t,soln, x_last, t_memory1, sp_memory1, So_memory1, t_memory2, sp_memory2, So_memory2, t_memory3, sp_memory3, So_memory3, t_memory4, sp_memory4, So_memory4, t_memory5, sp_memory5, So_memory5,   t_memory8, sp_memory8, So_memory8,    kla_memory1, kla_memory2, kla_memory3, kla_memory4, kla_memory5, kla_memory8,   Qeff,Qw,kla_memory3,kla_memory5,kla_memory8
     
     def step(self, action) :
         
@@ -145,7 +145,7 @@ class SbrEnv1(gym.Env):
         self._take_action(action)
     
     
-        t,soln, x_last, t_memory1, sp_memory1, So_memory1, t_memory2, sp_memory2, So_memory2, t_memory3, sp_memory3, So_memory3, t_memory4, sp_memory4, So_memory4, t_memory5, sp_memory5, So_memory5, t_memory8, sp_memory8, So_memory8, kla_memory1, kla_memory2, kla_memory3, kla_memory4, kla_memory5, kla_memory8, Qeff,Qw = self._next_observation(WV, IV, t_ratio, influent_mixed, DO_control_par, x, DO_setpoints)
+        t,soln, x_last, t_memory1, sp_memory1, So_memory1, t_memory2, sp_memory2, So_memory2, t_memory3, sp_memory3, So_memory3, t_memory4, sp_memory4, So_memory4, t_memory5, sp_memory5, So_memory5, t_memory8, sp_memory8, So_memory8, kla_memory1, kla_memory2, kla_memory3, kla_memory4, kla_memory5, kla_memory8, Qeff,Qw ,kla_memory3,kla_memory5,kla_memory8= self._next_observation(WV, IV, t_ratio, influent_mixed, DO_control_par, x, DO_setpoints)
                 
         
         reward =  sbr_reward(x_last,  DO_control_par, kla_memory_3, kla_memory_5, kla_memory_8, Qeff,Qw)
