a5627a3683e47f8365753afc9e7a3e976aa94d8c
SungKu <33339044+SungKu@users.noreply.github.com>
2019-12-04 15:15:49 +0900

    Update __init__.py

--- gym_SBR/envs/__init__.py
+++ gym_SBR/envs/__init__.py
@@ -1,3 +1,4 @@
 from gym_SBR.envs.gym_SBR_env0 import SbrEnv
 from gym_SBR.envs.gym_SBR_env1 import SbrEnv1
 from gym_SBR.envs.gym_SBR_env2 import SbrEnv2
+from gym_SBR.envs.gym_SBR_env4 import SbrEnv4
