39afb687e5c690f77d58bb8b69e8066382d48d10
SungKu <33339044+SungKu@users.noreply.github.com>
2019-12-06 15:30:15 +0900

    Update __init__.py

--- gym_SBR/__init__.py
+++ gym_SBR/__init__.py
@@ -4,3 +4,5 @@ register(id='SBR-v0',entry_point='gym_SBR.envs:SbrEnv',)
 register(id='SBR-v1',entry_point='gym_SBR.envs:SbrEnv1',)
 register(id='SBR-v2',entry_point='gym_SBR.envs:SbrEnv2',)
 register(id='SBR-v4',entry_point='gym_SBR.envs:SbrEnv4',)
+register(id='SBRCnt-v0',entry_point='gym_SBR.envs:SbrCnt0',)
+
